98ef2ae27fca66cc9c6195269c62ab1af30bab4b
Шарангия Игорь <igor_korenovsk@mail.ru>
2023-06-06 15:36:47 +0300

    skybox

---
+++ SolarSystem/SkyBox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Mathematics;
+
+namespace SolarSystem
+{
+    internal class SkyBox
+    {
+        public Mesh Mesh { get; set; }
+
+        public SkyBox(Mesh mesh)
+        {
+            Mesh = mesh;
+        }
+
+        public void DrawMehs()
+        {
+            Mesh.DrawMesh();
+        }
+    }
+}
--- SolarSystem/SolarSystemSimulation.cs
+++ SolarSystem/SolarSystemSimulation.cs
@@ -7,6 +7,7 @@ using SolarSystem;
 using System;
 using System.Drawing;
 using static OpenTK.Graphics.OpenGL.GL;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SolarSystemSimulation
 {
@@ -23,6 +24,7 @@ namespace SolarSystemSimulation
         private double _time;
         private Shader _shader;
         private Shader _shader2;
+        private SkyBox skyBox;
 
         private List<InterfaceObject> ObjectsList;
         public SolarSystemSimulation(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title })
@@ -46,7 +48,7 @@ namespace SolarSystemSimulation
             GL.Enable(EnableCap.TextureGenT);*/
 
 
-            GL.ClearColor(Color.Aqua);
+            GL.ClearColor(Color.Black);
             // We initialize the camera so that it is 3 units back from where the rectangle is.
             // We also give it the proper aspect ratio.
             //_camera = new Camera(Vector3.UnitZ * 3, Size.X / (float)Size.Y);
@@ -97,7 +99,7 @@ namespace SolarSystemSimulation
             ObjectsList.Add(new Planet(new ObjParser("../../../blender/saturn.obj").GetMesh(), 9.5388f, 0.0542f, 10755.7f, 568.0f, 9.69f, 26.73f, 10.66f));
             ObjectsList.Add(new Planet(new ObjParser("../../../blender/uranus.obj").GetMesh(), 19.1914f, 0.0472f, 30687.15f, 86.8f, 6.81f, 97.77f, -17.24f));
             ObjectsList.Add(new Planet(new ObjParser("../../../blender/neptune.obj").GetMesh(), 30.0611f, 0.0086f, 60190.03f, 102.0f, 5.43f, 28.32f, 16.11f));
-
+            skyBox = new SkyBox(new ObjParser("../../../blender/skybox.obj").GetMesh());
 
             // _meshes.Add(new ObjParser("../../../blender/test.obj").GetMech());
         }
@@ -119,6 +121,11 @@ namespace SolarSystemSimulation
             GL.LoadIdentity();
             var model = Matrix4.Identity;
 
+            _shader.Use();
+            _shader.SetMatrix4("view", _camera.GetViewMatrix());
+            _shader.SetMatrix4("projection", _camera.GetProjectionMatrix());
+            _shader.SetMatrix4("model", model);
+            skyBox.DrawMehs();
 
             //solarSystem.Draw();
             foreach (var objectel in ObjectsList)
