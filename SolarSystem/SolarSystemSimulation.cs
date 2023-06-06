f07e994bcbc24f408b916280d7606d7ac2c34eb6
Шарангия Игорь <igor_korenovsk@mail.ru>
2023-06-06 18:08:27 +0300

    изменил модельки и добавил солнце

--- SolarSystem/SolarSystemSimulation.cs
+++ SolarSystem/SolarSystemSimulation.cs
@@ -9,6 +9,7 @@ using System.Drawing;
 using static OpenTK.Graphics.OpenGL.GL;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using ErrorCode = OpenTK.Graphics.OpenGL.ErrorCode;
+using Sun = SolarSystem.Sun;
 
 namespace SolarSystemSimulation
 {
@@ -26,6 +27,7 @@ namespace SolarSystemSimulation
         private Shader _shader;
         private Shader _shader2;
         private SkyBox skyBox;
+        private Sun sun;
 
         private List<InterfaceObject> ObjectsList;
         public SolarSystemSimulation(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title })
@@ -92,15 +94,16 @@ namespace SolarSystemSimulation
         {
             /*   Mesh Sun = new ObjParser("../../../blender/sun.obj").GetMech(0.0f, 0.0f, 0.0f);
                _meshes.Add(Sun);   */
-            ObjectsList.Add(new Planet(new ObjParser("../../../blender/mercury.obj").GetMesh(), 0.3871f, 0.2056f, 87.97f, 0.330f, 47.87f, 0.03f, 58.64f));
-            ObjectsList.Add(new Planet(new ObjParser("../../../blender/venus.obj").GetMesh(), 0.7233f, 0.0068f, 224.7f, 4.87f, 35.02f, 177.36f, -243.02f));
-            ObjectsList.Add(new Planet(new ObjParser("../../../blender/earth.obj").GetMesh(), 1.0f, 0.0167f, 365.25f, 5.97f, 29.78f, 23.44f, 24.0f));
-            ObjectsList.Add(new Planet(new ObjParser("../../../blender/mars.obj").GetMesh(), 1.5237f, 0.0934f, 686.98f, 0.642f, 24.07f, 25.19f, 24.62f));
-            ObjectsList.Add(new Planet(new ObjParser("../../../blender/jupiter.obj").GetMesh(), 5.2026f, 0.0484f, 4332.82f, 1898.0f, 13.07f, 3.12f, 9.93f));
-            ObjectsList.Add(new Planet(new ObjParser("../../../blender/saturn.obj").GetMesh(), 9.5388f, 0.0542f, 10755.7f, 568.0f, 9.69f, 26.73f, 10.66f));
-            ObjectsList.Add(new Planet(new ObjParser("../../../blender/uranus.obj").GetMesh(), 19.1914f, 0.0472f, 30687.15f, 86.8f, 6.81f, 97.77f, -17.24f));
-            ObjectsList.Add(new Planet(new ObjParser("../../../blender/neptune.obj").GetMesh(), 30.0611f, 0.0086f, 60190.03f, 102.0f, 5.43f, 28.32f, 16.11f));
+            ObjectsList.Add(new Planet(new ObjParser("../../../blender/mercury.obj").GetMesh(), 0.3871f*3f, 0.2056f, 87.97f, 0.330f, 47.87f, 0.03f, 58.64f));
+            ObjectsList.Add(new Planet(new ObjParser("../../../blender/venus.obj").GetMesh(), 0.7233f * 3f, 0.0068f, 224.7f, 4.87f, 35.02f, 177.36f, -243.02f));
+            ObjectsList.Add(new Planet(new ObjParser("../../../blender/earth.obj").GetMesh(), 1.0f * 3f, 0.0167f, 365.25f, 5.97f, 29.78f, 23.44f, 24.0f));
+            ObjectsList.Add(new Planet(new ObjParser("../../../blender/mars.obj").GetMesh(), 1.5237f * 3f, 0.0934f, 686.98f, 0.642f, 24.07f, 25.19f, 24.62f));
+            ObjectsList.Add(new Planet(new ObjParser("../../../blender/jupiter.obj").GetMesh(), 5.2026f * 3f, 0.0484f, 4332.82f, 1898.0f, 13.07f, 3.12f, 9.93f));
+            ObjectsList.Add(new Planet(new ObjParser("../../../blender/saturn.obj").GetMesh(), 9.5388f * 3f, 0.0542f, 10755.7f, 568.0f, 9.69f, 26.73f, 10.66f));
+            ObjectsList.Add(new Planet(new ObjParser("../../../blender/uranus.obj").GetMesh(), 19.1914f * 3f, 0.0472f, 30687.15f, 86.8f, 6.81f, 97.77f, -17.24f));
+            ObjectsList.Add(new Planet(new ObjParser("../../../blender/neptune.obj").GetMesh(), 30.0611f * 3f, 0.0086f, 60190.03f, 102.0f, 5.43f, 28.32f, 16.11f));
             skyBox = new SkyBox(new ObjParser("../../../blender/skybox.obj").GetMesh());
+            sun = new Sun(new ObjParser("../../../blender/sun.obj").GetMesh());
 
             // _meshes.Add(new ObjParser("../../../blender/test.obj").GetMech());
         }
@@ -130,7 +133,7 @@ namespace SolarSystemSimulation
             _shader.SetMatrix4("projection", _camera.GetProjectionMatrix());
             _shader.SetMatrix4("model", model);
             skyBox.DrawMehs();
-
+            sun.DrawMesh();
             //solarSystem.Draw();
             foreach (var objectel in ObjectsList)
             {
